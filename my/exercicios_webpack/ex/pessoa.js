d47959333b47c335139b23334a9a547e566a9f34
earthW0rm <marco.a.faustino@gmail.com>
2018-03-26 23:49:53 -0300

    Colocando o webpack para rodar com css

---
+++ my/exercicios_webpack/ex/pessoa.js
@@ -0,0 +1,9 @@
+export default class Pessoa {
+    constructor(nome){
+        this.none = nome;
+    }
+
+    toString(){
+        return `Pessoa: ${this.none}`;
+    }
+}
--- my/exercicios_webpack/webpack.config.js
+++ my/exercicios_webpack/webpack.config.js
@@ -5,11 +5,24 @@ module.exports = {
     , output:{
         path: __dirname + '/public'
         , filename: './bundle.js'
-        
     }
     , devServer: {
         port: 8080
         , contentBase: './public'
-
     }
+    , module: {
+        rules: [
+          {
+            test: /.js?$/,
+            exclude: /(node_modules|bower_components)/,
+            use: {
+              loader: 'babel-loader',
+              options: {
+                presets: ['es2015', 'react']
+                , plugins: ['transform-object-rest-spread']
+              }
+            }
+          }
+        ]
+      }
 }
