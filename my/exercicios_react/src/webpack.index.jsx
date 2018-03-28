d673e009f74558af31a45403cab4e65066bdfc62
EarthW0rm <marco.a.faustino@gmail.com>
2018-03-28 18:54:29 -0300

    Say Hello do React, primeiros passos, webpack rodando

---
+++ my/exercicios_react/src/webpack.index.jsx
@@ -0,0 +1,4 @@
+import React from 'react';
+import ReactDom from 'react-dom';
+
+ReactDom.render(<h1>Say hello to my little friend</h1>, document.getElementById('app'));
---
+++ my/exercicios_react/webpack.config.js
@@ -0,0 +1,58 @@
+// var ExtractTextPlugin = require('extract-text-webpack-plugin');
+const HtmlWebPackPlugin = require("html-webpack-plugin");
+
+// var extractPlugin = new ExtractTextPlugin({
+//     filename: 'style.css'
+//     , allChunks: true
+// });
+
+module.exports = {
+    entry: './src/webpack.index.jsx'
+    , output:{
+        path: __dirname + '/dist'
+        , filename: 'bundle.js'
+        , publicPath: '/dist'
+    }
+    , module: {
+        rules: [
+            {
+                test: /\.jsx$/
+                , exclude: /(node_modules|bower_components)/
+                , use: [
+                    {
+                        loader: 'babel-loader'
+                        , options: {
+                            presets: ['es2015', 'react']
+                        }
+                    }
+                ]
+            }
+            ,{
+                test: /\.css$/,
+                use: ['style-loader', 'css-loader']
+            }
+            // , {
+            //     test: /\.scss$/
+            //     , use: extractPlugin.extract({
+            //         use: ['css-loader', 'sass-loader']
+            //     })
+            // }
+            ,{
+                test: /\.html$/,
+                use: [
+                  {
+                    loader: "html-loader",
+                    options: { minimize: true }
+                  }
+                ]
+            }
+        ]
+    }
+    , plugins: [
+        // extractPlugin
+        new HtmlWebPackPlugin({
+            template: "./src/index.html",
+            filename: "./index.html"
+        })
+    ]
+};
