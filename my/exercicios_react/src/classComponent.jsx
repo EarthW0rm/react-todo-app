f5d22b4c896d37874118659c4c80dd2a881338ff
EarthW0rm <marco.a.faustino@gmail.com>
2018-03-29 20:09:39 -0300

    Incluindo um componente com notacao de classe

---
+++ my/exercicios_react/src/classComponent.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+export default class ClassComponent extends React.Component {
+    
+    render() {
+        return(
+            <h1>{this.props.value}</h1>
+        );
+    }
+
+
+}
--- my/exercicios_react/src/webpack.index.jsx
+++ my/exercicios_react/src/webpack.index.jsx
@@ -2,13 +2,11 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/main.scss';
 import React from 'react';
 import ReactDom from 'react-dom';
-import Family from './family';
-import Member from './member';
-//import Griffins from './familyGuy';
+import ClassComponent from './classComponent';
 
 
 ReactDom.render(
     <div className="container">
-
+        <ClassComponent value="Say hello to my little friend!!!"/>
     </div>
     , document.getElementById('app'));
