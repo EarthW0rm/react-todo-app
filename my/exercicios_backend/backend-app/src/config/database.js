b4bbf59b64c6620abb474125fbaf3e7697a7c6c5
earthW0rm <marco.a.faustino@gmail.com>
2018-04-19 01:57:56 -0300

    Alteracoes 'HardCoded' para comunicar a appnode com o mongo, criacao de uma versao alpine

--- my/exercicios_backend/backend-app/src/config/database.js
+++ my/exercicios_backend/backend-app/src/config/database.js
@@ -1,4 +1,4 @@
 const mongoose = require('mongoose');
 mongoose.Promise = global.Promise;
 
-module.exports = mongoose.connect('mongodb://reactBack:senha123@localhost:27017');
+module.exports = mongoose.connect('mongodb://reactBack:senha123@172.17.0.3:27017',  { useMongoClient: true });
