ed4e05757d3d003afb62966d1de77f01539aff3f
EarthW0rm <marco.a.faustino@gmail.com>
2018-04-21 19:32:10 -0300

    Inclusao das funcoes o form

--- my/exercicios_backend/frontend-app/src/main/todo/todo.jsx
+++ my/exercicios_backend/frontend-app/src/main/todo/todo.jsx
@@ -5,11 +5,28 @@ import TodoList from './todo-list'
 
 export default class Todo extends Component{
 
+    constructor(props){
+        super(props);
+        this.handleAdd = this.handleAdd.bind(this);
+        this.handleChange = this.handleChange.bind(this);
+        this.state = {description: '', list: []};
+    }
+
+    handleAdd(evt){
+        console.log('Handle Add');
+    }
+
+    handleChange(evt){
+        this.setState({...this.state, description: evt.target.value});
+    }
+
     render(){
         return(
             <div>
                 <PageHeader name="Tarefas" small="Cadastro"/>
-                <TodoForm />
+                <TodoForm description={this.state.description}
+                    handleAdd={this.handleAdd}
+                    handleChange={this.handleChange}/>
                 <TodoList />
             </div>
         )
--- my/exercicios_backend/frontend-app/src/template/icon-button.jsx
+++ my/exercicios_backend/frontend-app/src/template/icon-button.jsx
@@ -1,9 +1,12 @@
 import React from 'react'
+import IF from './helper/if'
 
 export default props => {
     return(
-        <button className={`btn btn-${props.style}`}>
-            <i className={`fa fa-${props.icon}`}></i>
-        </button>
+        <IF test={!props.hide == true}>
+            <button onClick={props.onClick} className={`btn btn-${props.style}`}>
+                <i className={`fa fa-${props.icon}`}></i>
+            </button>
+        </IF>
     );
 }
