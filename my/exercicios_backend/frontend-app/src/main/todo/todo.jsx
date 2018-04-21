d7971fc784d53c73ffae0dc22878f924af9df45f
EarthW0rm <marco.a.faustino@gmail.com>
2018-04-21 15:09:31 -0300

    Adicionando o controle de todo

---
+++ my/exercicios_backend/frontend-app/src/main/todo/todo.jsx
@@ -0,0 +1,15 @@
+import React, { Component } from 'react'
+
+export default class Todo extends Component{
+
+    render(){
+        return(
+            <div>
+                <h1>
+                    Say Say...
+                </h1>
+            </div>
+
+        )
+    }
+}
