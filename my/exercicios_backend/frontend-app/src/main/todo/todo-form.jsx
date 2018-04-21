7ea14dcfb76fdc23e6da28b343de2939cda4e0d6
EarthW0rm <marco.a.faustino@gmail.com>
2018-04-21 17:59:50 -0300

    Efetuando a componentizacao de botoes

--- my/exercicios_backend/frontend-app/src/main/todo/todo-form.jsx
+++ my/exercicios_backend/frontend-app/src/main/todo/todo-form.jsx
@@ -1,11 +1,20 @@
 import React, {Component} from 'react'
+import Grid  from '../../template/grid'
+import IconButton from '../../template/icon-button'
 
 export default class TodoForm extends Component{
 
     render(){
         return(
-            <div>
-                <h3>Formulário</h3>
+            <div role="form" className="todo__todo-form row">
+                <Grid cols="12 9 10">
+                    <input type="text" name="" id="description" className="form-control"
+                        placeholder="Adicione uma tarefa"/>
+                </Grid>
+
+                <Grid  cols="12 3 2">
+                    <IconButton style="primary" icon="plus"/>
+                </Grid>
             </div>
         )
     }
