2f98d76d3701f4070ec73aec99720e9ab0a20cb0
EarthW0rm <marco.a.faustino@gmail.com>
2018-05-13 14:50:53 -0300

    Reestruturacao do repositorio espelhado

--- my/exercicios_react/garbage/family.jsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-
-export default props => (
-    <div>
-        <h2>Família</h2>
-        { React.Children.map(props.children, child => React.cloneElement(child, {...props}) ) }
-    </div>
-)
--- my/exercicios_webpack/ex/pessoa.js
+++
@@ -1,9 +0,0 @@
-export default class Pessoa {
-    constructor(nome){
-        this.none = nome;
-    }
-
-    toString(){
-        return `Pessoa: ${this.none}`;
-    }
-}
